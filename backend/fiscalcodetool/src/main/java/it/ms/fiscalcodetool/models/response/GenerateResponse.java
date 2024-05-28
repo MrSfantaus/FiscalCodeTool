60de9bfb53cb4086a06a2dbd797f0713bf37f101
Simone Mattei <104928094+MrSfantaus@users.noreply.github.com>
2024-05-28 14:47:58 +0200

    Update GenerateResponse.java

--- backend/fiscalcodetool/src/main/java/it/ms/fiscalcodetool/models/response/GenerateResponse.java
+++ backend/fiscalcodetool/src/main/java/it/ms/fiscalcodetool/models/response/GenerateResponse.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.ms.fiscalcodetool.models.dto.JsonDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @NoArgsConstructor
@@ -17,7 +16,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Generate response", example = "{'message': 'OK', 'success': true, 'data':{'fiscalCode': 'FSCLCD37T07H501J', 'homocodesFiscalCode': ['FSCLCD37T07H5L1J', 'FSCLCD37T07H50MJ']}}")
 public class GenerateResponse implements JsonDTO {
 
+    @Schema(description = "Fiscal Code", example = "FSCLCD37T07H501J")
     private String fiscalCode;
+
+    @Schema(description = "Homocoded fiscal codes", example = "['FSCLCD37T07H5L1J', 'FSCLCD37T07H50MJ']")
     private List<String> homocodesFiscalCode;
 
     @Override
